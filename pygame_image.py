5d01a677a7036237d7aaee4f881e413e9fa56848
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-13 16:47:25 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -5,13 +5,15 @@ import pygame as pg
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    #screen2 = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_imgrv = pg.transform.flip(bg_img,True,False) 
     tori_img = pg.image.load("fig/3.png")
     tori_img = pg.transform.flip(tori_img,True,False)#２番
     rotate_img = pg.transform.rotozoom(tori_img,10,1.0)
-    img_lst=[tori_img,rotate_img]
+    rotate2_img = pg.transform.rotozoom(tori_img,15,1.0)
+    rotate0_img = pg.transform.rotozoom(tori_img,355,1.0)
+    img_lst=[tori_img,rotate_img,rotate2_img,rotate_img,tori_img,rotate0_img]
     tmr = 0
     x = 0
     
@@ -20,12 +22,13 @@ def main():
             if event.type == pg.QUIT: return
         
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600 - x, 0])
-        screen.blit(img_lst[tmr%2],[300,200])
+        screen.blit(bg_imgrv, [1600 - x, 0])
+        screen.blit(bg_img, [3200 - x, 0])
+        screen.blit(img_lst[tmr%3],[300,200])
         pg.display.update()
-        tmr += 1 
-        x = tmr % 1600
-        clock.tick(1000)
+        tmr += 4 
+        x = tmr % 3200
+        clock.tick(500)
 
 
 if __name__ == "__main__":
