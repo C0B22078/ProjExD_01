69934301cb43515a2cf931d89ca833a329d512f3
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-13 15:10:58 +0900

    リスト作成

--- pygame_image.py
+++ pygame_image.py
@@ -4,11 +4,13 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((1500, 800))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img = pg.transform.flip(bg_img,True,False)
     tmr = 0
+    rotate_img = pg.transform.rotozoom(bg_img,10,1.0)
+    img_lst=[bg_img,rotate_img]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
