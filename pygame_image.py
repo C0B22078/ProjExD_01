c136a599d763c66fdd75fc8d220dfb03405ac899
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-13 15:19:27 +0900

    左右反転(2)

--- pygame_image.py
+++ pygame_image.py
@@ -4,18 +4,21 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1500, 800))
+    screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img = pg.transform.flip(bg_img,True,False)
+    tori_img = pg.image.load("fig/3.png")
+    tori_img = pg.transform.flip(tori_img,True,False)
     tmr = 0
     rotate_img = pg.transform.rotozoom(bg_img,10,1.0)
-    img_lst=[bg_img,rotate_img]
+    #img_lst=[bg_img,rotate_img]
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(tori_img[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
