3514bd8f8892839f97897bd9c37cce76a02e7666
C0B22078 <c0b220782d@edu.teu.ac.jp>
2023-06-13 17:33:30 +0900

    こうかとん　修正

--- pygame_image.py
+++ pygame_image.py
@@ -12,11 +12,13 @@ def main():
     tori_img = pg.transform.flip(tori_img,True,False)#２番
     rotate_img = pg.transform.rotozoom(tori_img,10,1.0)
     rotate2_img = pg.transform.rotozoom(tori_img,15,1.0)
-    rotate0_img = pg.transform.rotozoom(tori_img,355,1.0)
-    img_lst=[tori_img,rotate_img,rotate2_img,rotate_img,tori_img,rotate0_img]
+    rotate3_img = pg.transform.rotozoom(tori_img,20,1.0)
+    rotate4_img = pg.transform.rotozoom(tori_img,25,1.0)
+    img_lst=[tori_img,rotate_img,rotate2_img,rotate3_img,rotate4_img,rotate3_img,rotate2_img,rotate2_img,rotate_img]
     tmr = 0
     x = 0
-    
+    y = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -24,11 +26,12 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_imgrv, [1600 - x, 0])
         screen.blit(bg_img, [3200 - x, 0])
-        screen.blit(img_lst[tmr%3],[300,200])
+        screen.blit(img_lst[tmr%len(img_lst)],[300,200])
         pg.display.update()
-        tmr += 4 
+        tmr += 1
         x = tmr % 3200
-        clock.tick(500)
+        
+        clock.tick(10)
 
 
 if __name__ == "__main__":
